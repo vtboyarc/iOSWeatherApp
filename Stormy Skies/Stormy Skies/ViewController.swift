bea55b98c99714c7436e0eccf6b81adcbf1dd146
vtboyarc <adamrcarter@gmail.com>
2016-08-12 16:15:01 -0500

    first commit

---
+++ Stormy Skies/Stormy Skies/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Stormy Skies
+//
+//  Created by Adam Carter on 8/12/16.
+//  Copyright © 2016 Adam Carter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    @IBOutlet weak var currentHumidityLabel: UILabel!
+    @IBOutlet weak var currentPrecipitationLabel: UILabel!
+    @IBOutlet weak var currentWeatherIcon: UIImageView!
+    @IBOutlet weak var currentSummaryLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
