79ace24c4111224162d66e533a779534d8cfb4de
vtboyarc <adamrcarter@gmail.com>
2016-08-15 18:46:45 -0500

    breaking some code out into a protocol to be more Swift-like

--- Stormy Skies/Stormy Skies/APIClient.swift
+++ Stormy Skies/Stormy Skies/APIClient.swift
@@ -8,17 +8,26 @@
 
 import Foundation
 
-//added ARC in front to prevent naming collisions, which could come from third pary code I might use or from cocoa frameworks...thanks, objective C
+/*added ARC in front to prevent naming collisions, 
+ which could come from third pary code I might use 
+ or from cocoa frameworks...thanks, objective C
+ */
 public let ARCNetworkingErrorDomain = "io.adamcarter.StormySkies.NetworkingError"
 
 public let MissingHTTPResponseError: Int = 10
 
-//putting these typealieas's here to make the functions in the protocol below more readable
+/*putting these typealieas's here to make the functions 
+in the protocol below more readable
+ */
 typealias JSON = [String : AnyObject]
 typealias JSONTaskCompletion = (JSON?, NSHTTPURLResponse?, NSError?) -> Void
 typealias JSONTask = NSURLSessionDataTask
 
-//making it simple to switch on the result at the call type and know if it was a success or failure and take action depending on which value is returned
+/*
+ making it simple to switch on the result at 
+ the call type and know if it was a success or
+ failure and take action depending on which value is returned
+ */
 enum APIResult<T> {
     case Success(T)
     case Failure(ErrorType)
